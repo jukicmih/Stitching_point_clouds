039d5b5b9d4aa49ab611130d397db849f01598b0
mirnlovr <mirna.lovric1@gmail.com>
2023-06-05 17:07:26 +0200

    plotovi dodani

--- run.py
+++ run.py
@@ -1,80 +1,135 @@
+import open3d
 import os
 import numpy as np
-from Utils.pcd_cutter import _cull_point_cloud_in_torus_around_center
-from Algorithms._calculate_earth_mover_distances_from_point_clouds import _calculate_earth_mover_distances_from_point_clouds
-from Visualisation.visualize import visualize
+import matplotlib.pyplot as plt
+
+
+from Algorithms._calculate_earth_mover_distances_from_point_clouds import \
+    _calculate_earth_mover_distances_from_point_clouds
 from Utils.read_pcd import read_pcd
 from Algorithms.get_max_point_distance import get_max_point_distance
-import open3d.geometry
+from Algorithms.point_2_point import point_2_point
+# from open3d.geometry import estimate_normals
+from Visualisation.visualize import visualize
 
-username = "MihaelaJ"
-#file_path = "C:/Users/" + username + "/Desktop/zadatak/lidar_sample"
-file_path = "C:/Users/" + username + "/Desktop/quality_check/CCHA0B2202029903_SE-SNA170_20220714_230543_VERY_GOOD/Lidar/"
 
+username = "MirnaL"
+file_path = "C:/Users/" + username + "/Desktop/quality_check/CCHA0B2202029903_SE-SNA170_20220714_185346_BAD/Lidar/"
 mat_files = [f for f in os.listdir(file_path) if f.endswith(".mat")]
+print(mat_files)
 
-step = 20   # uzimamo svaki dvadeseti file
+rmse_res = []
+p2l_res = []
+emd_res = []
+downsample_rate = 100
+step = 20
 for i in range(0, len(mat_files) - step, step):
     one_array = np.empty((0, 3))
     point_cloud1 = read_pcd(file_path, mat_files[i])
-    visualize(point_cloud1)
+    #visualize(point_cloud1)
     point_cloud2 = read_pcd(file_path, mat_files[i + step])
-    visualize(point_cloud2)
+    #visualize(point_cloud2)
     one_array = np.vstack((one_array, point_cloud1.points))
     one_array = np.vstack((one_array, point_cloud2.points))
     new_pcd = open3d.geometry.PointCloud()
     new_pcd.points = open3d.utility.Vector3dVector(one_array)
-    visualize(new_pcd)
-    point_cloud1.estimate_normals()
+    #visualize(new_pcd)
+    normals = point_cloud1.estimate_normals()
     p2l_dist = get_max_point_distance(point_cloud1.points, point_cloud2.points, point_cloud1.normals)
-    suma = 0
-    suma = np.sum( p2l_dist * p2l_dist )
-    print("point to plane : ", suma)
+    suma = np.sum(p2l_dist*p2l_dist)
+    p2l_res.append(suma)
+    print("Point to plane:", suma)
+    downsampled_source1 = point_cloud1.points[0::downsample_rate] #gledamo svaku stotu tocku
+    downsampled_source2 = point_cloud2.points[0::downsample_rate]
+
+    new_pcd1 = open3d.geometry.PointCloud()
+    new_pcd1.points = open3d.utility.Vector3dVector(downsampled_source1)
+    new_pcd2 = open3d.geometry.PointCloud()
+    new_pcd2.points = open3d.utility.Vector3dVector(downsampled_source2)
+
+    suma1 = _calculate_earth_mover_distances_from_point_clouds(np.asarray(new_pcd1.points), np.asarray(new_pcd2.points))
+    emd_res.append(suma1)
+    print("earth mover's distance:", suma1)
+
+    trans_init = np.eye(4)# ne kuzim zasto mora biti 4
+    reg_p2p = point_2_point(point_cloud1, point_cloud2, trans_init, 0.02, 200)
+    rmse_res.append(reg_p2p.inlier_rmse)
+    print("point to point:", reg_p2p.inlier_rmse)
+
+plt.figure()
+plt.title("point to plane, E")
+plt.plot(p2l_res, 'g')
+plt.show()
+
+plt.figure()
+plt.title("earth mover's distance")
+plt.plot(emd_res, 'r')
+plt.show()
+
+plt.figure()
+plt.title("point to point, RMSE")
+plt.plot(rmse_res, 'b')
+plt.show()
 
 
-# one_array = np.empty((0, 3))
-#   pcd = read_pcd("C:/Users/MihaelaJ/Desktop", "CCHA0B2202029903_SE-SNA170_20220714_194416_10_10_1341863041177833_agg.mat")
 
-# j = 0
-# for filename in mat_files:
-#     j += 1
-#     if(j == 1):
-#         point_cloud = read_pcd(file_path, filename)
-#         #pcd = open3d.io.read_point_cloud(file_path + filename)
-#     if(j == 2):
-#         point_cloud2 = read_pcd(file_path, filename)
-#         #pcd2 = open3d.io.read_point_cloud(file_path + filename)
-#         break
-#     # one_array = np.vstack((one_array, point_cloud))
-
+# normals = point_cloud1.estimate_normals()
+# print(point_cloud1.has_normals())
+# visualize(point_cloud1)
 #
-# normals = point_cloud.estimate_normals()
-# print(point_cloud.has_normals())
-# visualize(point_cloud)
-
-# p2l_dist = get_max_point_distance(point_cloud.points, point_cloud2.points, point_cloud.normals)
-#
+# p2l_dist = get_max_point_distance(point_cloud1.points, point_cloud2.points, point_cloud1.normals)
+# print(p2l_dist)
+# print(len(p2l_dist))
 # suma = 0
-# suma = np.sum( p2l_dist * p2l_dist )
+# # for i in range(len(p2l_dist)):
+# #     suma += p2l_dist(i)^2
+#
+# suma = np.sum(p2l_dist*p2l_dist)
+# print("Point to plane:")
 # print(suma)
 #
-# centar = point_cloud.get_center()
-# print(centar)
+# temp_array1 = np.asarray(point_cloud1.points)
+# temp_array2 = np.asarray(point_cloud2.points)
+#
+# print(temp_array1.shape)
+# print(temp_array2.shape)
 
-
-# cut_pcd = _cull_point_cloud_in_torus_around_center(point_cloud, centar, 15, 20)
-# cut_pcd.points = open3d.utility.Vector3dVector(np.array(cut_pcd.points)+np.array([0, 50, 0]))
-# open3d.visualization.draw_geometries([point_cloud, cut_pcd])
-# visualize(cut_pcd)
-
-# downsample_rate = 50
-# downsampled_source = point_cloud.points[0::downsample_rate]
+#downsampleiranje
+# downsample_rate = 100
+# downsampled_source1 = point_cloud1.points[0::downsample_rate] #gledamo svaku desetu tocku
 # downsampled_source2 = point_cloud2.points[0::downsample_rate]
 #
-# new_pcd = open3d.geometry.PointCloud()
-# new_pcd.points = open3d.utility.Vector3dVector(downsampled_source)
+# new_pcd1 = open3d.geometry.PointCloud()
+# new_pcd1.points = open3d.utility.Vector3dVector(downsampled_source1)
 # new_pcd2 = open3d.geometry.PointCloud()
 # new_pcd2.points = open3d.utility.Vector3dVector(downsampled_source2)
 
-#udaljenost = _calculate_earth_mover_distances_from_point_clouds(np.asarray(new_pcd.points), np.asarray(new_pcd2.points))
-#print(udaljenost)
+# suma1 = _calculate_earth_mover_distances_from_point_clouds(np.asarray(new_pcd1.points), np.asarray(new_pcd2.points))
+# print(suma1)
+
+# center = point_cloud1.get_center()
+# print(center)
+# print(center.shape)
+# center_R = np.reshape(center, (1, 3))
+#
+# print(center_R)
+# print(center_R.shape)
+#
+# isti_array = np.empty((100, 3))
+# for i in range(100):
+#     isti_array[i, :] = center_R
+#
+# print(isti_array)
+# # new_pcd = open3d.geometry.PointCloud()
+# # new_pcd.points = open3d.utility.Vector3dVector(isti_array)
+# new_pcd = open3d.geometry.PointCloud(open3d.utility.Vector3dVector(isti_array))
+# visualize(new_pcd)
+#
+# pcd = open3d.geometry.PointCloud()
+# pcd.points = open3d.utility.Vector3dVector(np.array(new_pcd.points) + np.array([0, 5, 0]))
+#
+# open3d.visualization.draw_geometries([new_pcd, pcd])
+#
+# # mesh = open3d.geometry.create_mesh_coordinate_frame(origin = one_array)
+# #visualize(one_array)
+# #trans_init = I
